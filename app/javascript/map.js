0c0ef3862397057f183fc58dfe5ee8155b5b45cc
krywna <krywnakrywna@gmail.com>
2023-01-13 08:15:24 +0300

    fix code style

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -5,31 +5,31 @@ function init(){
 
   if (!mapDiv) return
 
-    const address = mapDiv.getAttribute("data-address");
+  const address = mapDiv.getAttribute("data-address");
 
-    const myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 10
-    });
+  const myMap = new ymaps.Map("map", {
+      center: [55.76, 37.64],
+      zoom: 10
+  });
 
-    const myGeocoder = ymaps.geocode(address);
+  const myGeocoder = ymaps.geocode(address);
 
-    myGeocoder.then(
-      function (res) {
-        const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+  myGeocoder.then(
+    function (res) {
+      const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
-        myMap.geoObjects.add(
-            new ymaps.Placemark(
-              coordinates,
-              {iconContent: address},
-              {preset: "islands#blueStretchyIcon"}
-            )
-        );
+      myMap.geoObjects.add(
+          new ymaps.Placemark(
+            coordinates,
+            {iconContent: address},
+            {preset: "islands#blueStretchyIcon"}
+          )
+      );
 
-        myMap.setCenter(coordinates);
-        myMap.setZoom(15);
-      }, function (err) {
-        alert("Не можем найти такой адрес");
-      }
-    );
+      myMap.setCenter(coordinates);
+      myMap.setZoom(15);
+    }, function (err) {
+      alert("Не можем найти такой адрес");
+    }
+  );
 }
